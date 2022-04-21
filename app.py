4803599b8a6ae3d4ee4aa6b4f6914af4c36d58c6
Jossian <A01746745@itesm.mx>
2022-04-21 18:49:51 -0500

    cambios

--- app.py
+++ app.py
@@ -1,12 +1,24 @@
 from datetime import datetime
 from flask import Flask, render_template, request, session, url_for, redirect
 import datetime
+import pymongo
 # FlASK
 #############################################################
 app = Flask(__name__)
 app.permanent_session_lifetime=datetime.timedelta(days=365)
 app.secret_key="super secret key"
 #############################################################
+
+#Conectarnos a la base de datos
+#############################################################
+mongodb_key="mongodb+srv://desarrollowebuser:<password>@cluster0.x11il.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+client=pymongo.MongoClient(mongodb_key, tls=True, tlsAllowInvalidCertificates=True) #crea un cliente
+db=client.Escuela #conecta a la base de datos escuela
+cuentas=db.Alumno
+
+#############################################################
+
+
 
 @app.route('/')
 def home():
@@ -46,3 +58,13 @@ def logout():
         return redirect(url_for("home"))
     else :
         return redirect(url_for("home"))
+
+
+@app.route("\datos")
+def usuarios():
+    cursor=cuentas.find({})     #nos permite encontrar todo lo que haya en el documento (Alumnos)
+    #for para indagar
+    users=[]
+    for doc in cursor:
+        users.append(doc)
+    return render_template("\usuarios.html", data=users)    #crea template
